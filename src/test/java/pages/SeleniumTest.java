876c36202bfad079a3a94b7f29adae501dde008d
jacoboQA <lliacobofs@gmail.com>
2024-06-06 19:42:04 -0500

    adding selenium webdriver init

--- src/test/java/pages/SeleniumTest.java
+++ src/test/java/pages/SeleniumTest.java
@@ -1,8 +1,14 @@
-package test.java.pages;
+package pages;
 
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 
-public class seleniumTest {
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SeleniumTest {
     private WebDriver driver;
 
     @BeforeMethod
